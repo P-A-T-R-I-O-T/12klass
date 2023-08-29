4bf3cd540278faf160aa5b6d3f059b484c54df94
KonstantinKolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2023-08-29 06:32:12 +0300

    нашол по какому критерию вести сортировку

--- my_directory/my file.py
+++ my_directory/my file.py
@@ -1,3 +1,8 @@
+# Создать массив данных и сделать внутри этого массива поиск по Казани, России и  Миру
+# Просмотреть внутри массива, сколько приходится на навыки.
+
+
+
 import requests
 import pprint
 
@@ -6,18 +11,50 @@ DOMAIN = 'https://api.hh.ru/'
 
 url_vacancies = f'{DOMAIN}vacancies'
 
-params = {
-    'text': 'Python',
-    # есть страницы т.к. данных много
-    'page': 1
-}
 
+def search ():
+    # Создал поиск по миру
+    params_world = {
+        'text': 'NAME:Python',
+        # создал страницу т.к. данных много
+        'page': 1
+    }
+
+    # Создал поиск по России
+    params_russia = {
+        'text': '(NAME:Python) AND russia',
+        # создал страницу т.к. данных много
+        'page': 1
+    }
+
+
+    # Создал поиск по Казани
+    params_kazan = {
+        'text': '(NAME:Python) AND kazan',
+        # создал страницу т.к. данных много
+        'page': 1
+    }
+
+    params = {
+        'text': 'NAME: (Python) AND kazan',
+        # страница
+        'page': 1
+    }
+
+params= {
+        'text': 'NAME:Python',
+        # создал страницу т.к. данных много
+        'page': 1
+    }
 result = requests.get(url_vacancies, params=params).json()
-#print(result.status_code)
-#pprint.pprint(result.json())
+
+# print(result.status_code)
+# pprint.pprint(result.json())
 
 items = result['items']
 
-first = items[0]
+#first = items[0]
 
-pprint.pprint(first)
+pprint.pprint(items)
+# Нужно осуществить сортировку по 'area':('name': ''Казань'')
+'area': {'id': '1', 'name': 'Москва', 'url': 'https://api.hh.ru/areas/1'}
